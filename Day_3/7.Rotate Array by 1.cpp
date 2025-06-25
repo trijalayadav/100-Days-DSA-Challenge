3c707a3ccfe61ae184c61af9139447034e60de93
trijala-product-manager <trijala1234@gmail.com>
2025-06-25 22:09:38 +0530

    change in rotate array  approach

--- Day_3/7.Rotate Array by 1.cpp
+++ Day_3/7.Rotate Array by 1.cpp
@@ -1,13 +1,27 @@
-// User function Template for C++
+// // User function Template for C++
+
+// class Solution {
+//   public:
+//     void rotate(vector<int> &arr) {
+//         // code here
+//         vector<int> temp(arr.size());
+//         for(int i=0;i<arr.size();i++){
+//             temp[(i+1)%arr.size()]=arr[i];         
+//         }
+//         arr=temp;
+//     }
+// };
 
 class Solution {
   public:
     void rotate(vector<int> &arr) {
-        // code here
-        vector<int> temp(arr.size());
-        for(int i=0;i<arr.size();i++){
-            temp[(i+1)%arr.size()]=arr[i];         
+        int n = arr.size();
+        if (n == 0) return;
+
+        int last = arr[n - 1];
+        for (int i = n - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
         }
-        arr=temp;
+        arr[0] = last;
     }
 };
