6c24f068d3d5023e0fcbef2e93cefaa71dada35c
trijala-product-manager <trijala1234@gmail.com>
2025-07-22 21:09:23 +0530

    day 29 done

---
+++ Day_29/Maximum Sum Circular Subarray.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int total = 0;
+        int maxSum = nums[0], curMax = 0;
+        int minSum = nums[0], curMin = 0;
+
+        for (int num : nums) {
+            curMax = max(curMax + num, num);
+            maxSum = max(maxSum, curMax);
+
+            curMin = min(curMin + num, num);
+            minSum = min(minSum, curMin);
+
+            total += num;
+        }
+
+        if (maxSum < 0)
+            return maxSum;  
+
+        return max(maxSum, total - minSum);
+    }
+};
