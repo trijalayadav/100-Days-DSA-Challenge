24a74acb7c80646228156fdebeede1e050653975
trijala-product-manager <trijala1234@gmail.com>
2025-08-06 20:38:57 +0530

    day 43

---
+++ Day_43/5. Longest Palindromic Substring.cpp
@@ -0,0 +1,48 @@
+class Solution
+{
+public:
+    string longestPalindrome(string s)
+    {
+        int n = s.size();
+        if (n == 0)
+            return "";
+
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+
+        int maxLength = 1;
+        int start = 0;
+
+        for (int i = 0; i < n; ++i)
+            dp[i][i] = true;
+
+        for (int i = 0; i < n - 1; ++i)
+        {
+            if (s[i] == s[i + 1])
+            {
+                dp[i][i + 1] = true;
+                start = i;
+                maxLength = 2;
+            }
+        }
+
+        for (int len = 3; len <= n; ++len)
+        {
+            for (int i = 0; i <= n - len; ++i)
+            {
+                int j = i + len - 1;
+
+                if (s[i] == s[j] && dp[i + 1][j - 1])
+                {
+                    dp[i][j] = true;
+                    if (len > maxLength)
+                    {
+                        start = i;
+                        maxLength = len;
+                    }
+                }
+            }
+        }
+
+        return s.substr(start, maxLength);
+    }
+};
