2af20650be1c09e3784eb54b48a28c114cc5ce80
trijala-product-manager <trijala1234@gmail.com>
2025-08-18 18:46:00 +0530

    day 55

---
+++ Day_55/1547. Minimum Cost to Cut a Stick.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    int minCost(int n, vector<int> &cuts)
+    {
+        cuts.push_back(0);
+        cuts.push_back(n);
+        sort(cuts.begin(), cuts.end());
+
+        int m = cuts.size();
+        vector<vector<int>> dp(m, vector<int>(m, 0));
+
+        for (int len = 2; len < m; len++)
+        {
+            for (int i = 0; i + len < m; i++)
+            {
+                int j = i + len;
+                dp[i][j] = INT_MAX;
+
+                for (int k = i + 1; k < j; k++)
+                {
+                    dp[i][j] = min(dp[i][j],
+                                   cuts[j] - cuts[i] + dp[i][k] + dp[k][j]);
+                }
+            }
+        }
+
+        return dp[0][m - 1];
+    }
+};
